483ed5afea99a195de4da03f338a421a93f8697e
Nox Dineen <noxdineen@gmail.com>
2012-02-29 15:07:36 -0500

    Pushing broken carousel.js so I can diff it with Marcos version

--- static/scripts/carousel.js
+++ static/scripts/carousel.js
@@ -1,1 +1,8 @@
-# Super pimp jQuery moves
+$(function(){
+	var view_width = $(window).width()-20;
+	$('#carousel').css('width', 'view_width');
+	$('carousel ul').attr('style', '-webkit-transform: translate3d(1000px|0px|0px)');
+	$('#carousel .next').click(function(){
+		alert('Hey');
+	})
+});
