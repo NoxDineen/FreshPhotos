e7971ac618ccac22f3e3e2942aef4af7a6ff8f44
Nox Dineen <noxdineen@gmail.com>
2012-02-29 15:08:09 -0500

    Replaced carousel.js with cut and paste semi fixed version from Marcos

--- static/scripts/carousel.js
+++ static/scripts/carousel.js
@@ -1,8 +1,26 @@
-$(function(){
-	var view_width = $(window).width()-20;
-	$('#carousel').css('width', 'view_width');
-	$('carousel ul').attr('style', '-webkit-transform: translate3d(1000px|0px|0px)');
-	$('#carousel .next').click(function(){
-		alert('Hey');
-	})
+// calculate the screen width and apply to carrousel
+$(window).load(function(){
+    var view_width = $(window).width() - 20;
+
+    $('#carousel').css('width', view_width);
+
+    $('#carousel ul li:first-child').addClass('active');
+
+    var img_width = $('#carousel ul li.active').width();
+
+    var left = (view_width - img_width) / 2;
+
+    // move ul to the intial position
+    $('#carousel ul').attr('style', '-webkit-transform: translate3d('+left+'px, 0px, 0px);');
+
+    // attach event to the carrousel
+    $('#carousel .next').click(function() {
+        var img_width = $('#carousel ul li.active').next().width();
+        var left = ((view_width - img_width) / 2) - $('#carousel ul li.active').width();
+
+        $('#carousel ul').addClass('animate');
+        var new_position = "translate3d(" + left + "px, 0px, 0px)";
+        $('#carousel ul').css('-webkit-transform', new_position);
+        $('#carousel ul li.active').removeClass('active').next().addClass('active');
+    });
 });
